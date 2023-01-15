90eccc700e72f7aa47f785e1c9153b64ba533a27
Amandeep Singh Saluja <amandeepsaluja25@gmail.com>
2023-01-15 11:54:09 +0530

    class-6

---
+++ class-6/src/components/Body.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import { cards as RestaurantList } from "../../data.json";
+import RestrauntCard from "./Restaurant";
+import Shimmer from "./Shimmer";
+
+// What is state?
+// What is React Hooks? - functions.
+// What is
+function filterData(searchText, restaurants) {
+  return restaurants.filter((restaurant) =>
+    restaurant?.data?.name?.toLowerCase().includes(searchText?.toLowerCase())
+  );
+}
+
+const Body = () => {
+  const [allRestraunts, setAllRestraunts] = useState([]);
+
+  // searchText is a local state variable
+  const [searchTxt, setSearchTxt] = useState(""); // To create state variable
+
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  // -> empty dependency array -> once after first render
+  // -> dep arrat [searchTxt] => once after initial render + everytime after render ( my searchTxt changes)
+  useEffect(() => {
+    // API call
+    getRestraunts();
+    console.log("useEffect called.");
+  }, []);
+
+  async function getRestraunts() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.613322&lng=73.800667&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    //Optional Chaining
+    //console.log(json?.data?.cards[2]?.data?.data?.cards);
+    setAllRestraunts(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  console.log("render");
+
+  //not render component (Early return)
+  if (!allRestraunts) return null;
+
+  // Conditional Rendering
+  // if restraunt is empty => shimmer UI
+  // if restraunt has data => actual data UI
+
+  return allRestraunts.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchTxt}
+          onChange={(e) => {
+            // console.log(e.target.value);
+            setSearchTxt(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            //need to filter the data
+            const data = filterData(searchTxt, allRestraunts);
+            //update the state - restaurants
+            setAllRestraunts(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {/* write  */}
+        {filteredRestaurants.map((restaurant) => (
+          <RestrauntCard {...restaurant.data} key={restaurant.data.uuid} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Body;
