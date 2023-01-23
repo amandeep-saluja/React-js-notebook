2159411876cf4db1331a94bc03e90cef79d67b96
Amandeep Singh Saluja <amandeepsaluja25@gmail.com>
2023-01-24 00:35:31 +0530

    class-9 notes

--- class-9/src/hooks/useRestaurant.js
+++ class-9/src/hooks/useRestaurant.js
@@ -1,22 +1,20 @@
-import {useEffect, useState} from "react";
-import {RESTAURANT_DETAILS_URL} from "../constants";
+import { useEffect, useState } from "react";
+import { RESTAURANT_DETAILS_URL } from "../constants";
 
 const useRestaurant = (id) => {
+  const [restaurant, setRestaurant] = useState(null);
 
-    const [restaurant, setRestaurant] = useState(null);
+  useEffect(() => {
+    getRestaurantDetails();
+    console.log("use effect");
+  }, []);
 
-    useEffect(() => {
-        getRestaurantDetails();
-        console.log("use effect");
-    }, []);
+  async function getRestaurantDetails() {
+    const data = await fetch(RESTAURANT_DETAILS_URL + id);
+    const json = await data.json();
+    setRestaurant(json?.data);
+  }
 
-    async function getRestaurantDetails() {
-        const data = await fetch(RESTAURANT_DETAILS_URL + id);
-        const json = await data.json();
-        setRestaurant(json?.data);
-    }
-
-    return restaurant;
-}
-
+  return restaurant;
+};
 export default useRestaurant;
